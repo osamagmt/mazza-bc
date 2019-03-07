fd23e722ea9a974c9b3b0cdf6bf9ac5dc6fbd380
osamagmt <34580348+osamagmt@users.noreply.github.com>
2019-03-07 23:49:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,20 +2,20 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 const VIP = ['460976885036220426','532592989789487104'];
-const adminprefix = "$";
+const prefix = "$";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
 if (!VIP.includes(message.author.id)) return;
-if (message.content.startsWith(adminprefix + 'setname')) {
+if (message.content.startsWith(prefix + 'setname')) {
   client.user.setUsername(argresult).then
 	  message.channel.sendMessage(`Username Changed To **${argresult}**`)
   return message.reply("You Can change the username 2 times per hour");
 } else
-if (message.content.startsWith(adminprefix + 'setavatar')) {
+if (message.content.startsWith(prefix + 'setavatar')) {
   client.user.setAvatar(argresult);
    message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
 } else
-  if (message.content.startsWith(adminprefix + 'st')) {
+  if (message.content.startsWith(prefix + 'st')) {
     client.user.setGame(argresult, "https://www.twitch.tv/osama_gmt");
       message.channel.send(``)
   }
@@ -25,7 +25,7 @@ client.on('ready', () => {
 console.log('---------------------------------');
 console.log(`  # Bot Name:  ||   ${client.user.tag}`);
 console.log('---------------------------------');
-console.log(`  # Prefix:    ||   ${prefixbc}`);
+console.log(`  # Prefix:    ||   ${prefix}`);
 console.log('---------------------------------');
 console.log(`  # Id:        ||   ${client.user.id}`);
 console.log('--------------------------------');
@@ -74,7 +74,7 @@ client.on("message", async message => {
                                         });
 message.guild.members.forEach(m => { 
 m.send(args.replace('[user]', m));
-}
+})
                         });
                         no.on("collect", v => {
                             m.delete();
@@ -116,7 +116,7 @@ m.send(args.replace('[user]', m));
                                         });
 message.guild.members.forEach(m => { 
 m.send(args.replace('[user]', m));
-}
+})
                         });
                         no.on("collect", v => {
                             m.delete();
@@ -126,12 +126,13 @@ m.send(args.replace('[user]', m));
                         });
             }
 
-if(message.content.startsWith(prefixbc + "help")) {
-const embed = new Discord.RichEmbed() .setColor("RANDOM").setDescription(`**${prefixbc}obc ⇏ لإرسال رسالة إلى جميع أعضاء السيرفر
-${prefixbc}bc ⇏ لإرسال رسالة إلى الأعضاء الأونلاين فقط
+if(message.content.startsWith(prefix + "help")) {
+const embed = new Discord.RichEmbed() .setColor("RANDOM").setDescription(`**${prefix}obc ⇏ لإرسال رسالة إلى جميع أعضاء السيرفر
+${prefix}bc ⇏ لإرسال رسالة إلى الأعضاء الأونلاين فقط
 __تنبية__ : إذا أردت أن تمنشن العضو فقط أكتب بالرسالة
 \`[user]\` وسيقوم بإستبدالها بمنشن العضو**`);
 message.channel.sendEmbed(embed)}
+
 });
-});
+
 client.login(process.env.BOT_TOKEN);
