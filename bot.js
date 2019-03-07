265ce198cbb7602e2741d66ad2d3b5af399509b0
osamagmt <34580348+osamagmt@users.noreply.github.com>
2019-03-07 23:46:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,38 +36,102 @@ console.log('---------------------------------');
 console.log(`  # Channels:  ||   ${client.channels.size}`);
 console.log('---------------------------------');
 });
-const prefixbc = "$";
-client.on('message', async message => {  
-if(message.author.bot) return;
-if(message.channel.type === 'dm') return;
-if(!message.guild.members.get(message.author.id).hasPermission('ADMINISTRATOR')) return;
-const args = message.content.split(" ").slice(1);
-if(message.content.startsWith(prefixbc + 'obc')) {  
-message.channel.send(`**:loudspeaker:  تم ارسال هذة الرسالة الى __${message.guild.memberCount}__ مشترك**`);
+client.on("message", async message => {
+    var command = message.content.split(" ")[0];
+    command = command.slice(prefix.length);
+        if(!message.channel.guild) return;
+            var args = message.content.split(" ").slice(1).join(" ");
+            if(command == "bc") {
+                if(!message.member.hasPermission("ADMINISTRATOR")) {
+                    return message.channel.send("**للأسف لا تمتلك صلاحية `ADMINISTRATOR`**");
+                }
+                    if(!args) {
+                        return message.reply("**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**");
+                    }
+                        message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟\nمحتوى البرودكاست: \`${args}\`**`).then(m => {
+                            m.react("✅")
+                            .then(() => m.react("❌"));
+
+                            let yesFilter = (reaction, user) => reaction.emoji.name == "✅" && user.id == message.author.id;
+                            let noFiler = (reaction, user) => reaction.emoji.name == "❌" && user.id == message.author.id;
+
+                            let yes = m.createReactionCollector(yesFilter);
+                            let no = m.createReactionCollector(noFiler);
+
+                            yes.on("collect", v => {
+                                m.delete();
+                                    message.channel.send(`:ballot_box_with_check: | Done ... The Broadcast Message Has Been Sent For ${message.guild.memberCount} Members`).then(msg => msg.delete(5000));
+                                        message.guild.members.forEach(member => {
+                                            let bc = new Discord.RichEmbed()
+                                            .setColor("RANDOM")
+                                            .setThumbnail(message.author.avatarURL)
+                                            .setTitle("Broadcast")
+                                            .addField("Server", message.guild.name)
+                                            .addField("Sender", message.author.username)
+                                            .addField("Message", args);
+
+                                            member.sendEmbed(bc);
+                                        });
 message.guild.members.forEach(m => { 
 m.send(args.replace('[user]', m));
-});}
-if(message.content.startsWith(prefixbc + 'bc')) {  
-message.channel.send(`**:loudspeaker:  تم ارسال هذة الرسالة الى ${message.guild.members.filter(m => m.presence.status !== 'online').size}__ مشترك**`);
-message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+}
+                        });
+                        no.on("collect", v => {
+                            m.delete();
+                            message.channel.send("**Broadcast Canceled.**").then(msg => msg.delete(3000))
+                        });
+                            
+                        });
+            }
+            if(command == "bco") {
+                if(!message.member.hasPermission("ADMINISTRATOR")) {
+                    return message.channel.send("**للأسف لا تمتلك صلاحية `ADMINISTRATOR`**");
+                }
+                    if(!args) {
+                        return message.reply("**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**");
+                    }
+                        message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟\nمحتوى البرودكاست: \`${args}\`**`).then(m => {
+                            m.react("✅")
+                            .then(() => m.react("❌"));
+
+                            let yesFilter = (reaction, user) => reaction.emoji.name == "✅" && user.id == message.author.id;
+                            let noFiler = (reaction, user) => reaction.emoji.name == "❌" && user.id == message.author.id;
+
+                            let yes = m.createReactionCollector(yesFilter);
+                            let no = m.createReactionCollector(noFiler);
+
+                            yes.on("collect", v => {
+                                m.delete();
+                                    message.channel.send(`:ballot_box_with_check: | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.filter(r => r.presence.status !== "offline").size} Members`).then(msg => msg.delete(5000));
+                                        message.guild.members.filter(r => r.presence.status !== "offline").forEach(member => {
+                                            let bco = new Discord.RichEmbed()
+                                            .setColor("RANDOM")
+                                            .setThumbnail(message.author.avatarURL)
+                                            .setTitle("Broadcast")
+                                            .addField("Server", message.guild.name)
+                                            .addField("Sender", message.author.username)
+                                            .addField("Message", args);
+
+                                            member.sendEmbed(bco);
+                                        });
+message.guild.members.forEach(m => { 
 m.send(args.replace('[user]', m));
-});}
-if(message.content.startsWith(prefixbc + 'obc')) {  
-var role = message.mentions.roles.first();
-if(!role) {message.reply("لا توجد رتبة بهذا الاسم");return;}
-if(!args[0]) {
-message.channel.send(`قم بمنشنة الرتبة  | ${prefixbc}bcrole @admin message`);
-return;}
-message.channel.send(`**:loudspeaker:  تم ارسال هذة الرسالة الى __${role.members.size}__ مشترك**`);
-message.guild.members.filter(m => m.roles.get(role.id)).forEach(n => {
-n.send(args[1].replace('[user]', n));});}
+}
+                        });
+                        no.on("collect", v => {
+                            m.delete();
+                            message.channel.send("**Broadcast Canceled.**").then(msg => msg.delete(3000));
+                        });
+                            
+                        });
+            }
+
 if(message.content.startsWith(prefixbc + "help")) {
 const embed = new Discord.RichEmbed() .setColor("RANDOM").setDescription(`**${prefixbc}obc ⇏ لإرسال رسالة إلى جميع أعضاء السيرفر
 ${prefixbc}bc ⇏ لإرسال رسالة إلى الأعضاء الأونلاين فقط
-${prefixbc}bcrole ⇏  لإرسآل رسالة لرتبة محدده 
-ex :   ${prefixbc}bcrole @admin message
 __تنبية__ : إذا أردت أن تمنشن العضو فقط أكتب بالرسالة
 \`[user]\` وسيقوم بإستبدالها بمنشن العضو**`);
 message.channel.sendEmbed(embed)}
+});
 });
 client.login(process.env.BOT_TOKEN);
