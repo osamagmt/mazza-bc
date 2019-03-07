2cc960baf04fb5f710735016518c52d4b682a406
osamagmt <34580348+osamagmt@users.noreply.github.com>
2019-03-08 00:08:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,103 +36,72 @@ console.log('---------------------------------');
 console.log(`  # Channels:  ||   ${client.channels.size}`);
 console.log('---------------------------------');
 });
-client.on("message", async message => {
-    var command = message.content.split(" ")[0];
-    command = command.slice(prefix.length);
-        if(!message.channel.guild) return;
-            var args = message.content.split(" ").slice(1).join(" ");
-            if(command == "bc") {
-                if(!message.member.hasPermission("ADMINISTRATOR")) {
-                    return message.channel.send("**للأسف لا تمتلك صلاحية `ADMINISTRATOR`**");
-                }
-                    if(!args) {
-                        return message.reply("**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**");
-                    }
-                        message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟\nمحتوى البرودكاست: \`${args}\`**`).then(m => {
-                            m.react("✅")
-                            .then(() => m.react("❌"));
+const VIP = ['460976885036220426','532592989789487104'];
+const prefix = "-";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+if (!VIP.includes(message.author.id)) return;
+if (message.content.startsWith(prefix + 'setname')) {
+  client.user.setUsername(argresult).then
+	  message.channel.sendMessage(`Username Changed To **${argresult}**`)
+  return message.reply("You Can change the username 2 times per hour");
+} else
+if (message.content.startsWith(prefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
+} else
+  if (message.content.startsWith(prefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/osama_gmt");
+      message.channel.send(``)
+  }
+});
 
-                            let yesFilter = (reaction, user) => reaction.emoji.name == "✅" && user.id == message.author.id;
-                            let noFiler = (reaction, user) => reaction.emoji.name == "❌" && user.id == message.author.id;
+client.on('ready', () => {
+console.log('---------------------------------');
+console.log(`  # Bot Name:  ||   ${client.user.tag}`);
+console.log('---------------------------------');
+console.log(`  # Prefix:    ||   ${prefix}`);
+console.log('---------------------------------');
+console.log(`  # Id:        ||   ${client.user.id}`);
+console.log('--------------------------------');
+console.log(`  # Servers:   ||   ${client.guilds.size}`);
+console.log('--------------------------------');
+console.log(`  # Members:   ||   ${client.users.size}`);
+console.log('---------------------------------');
+console.log(`  # Channels:  ||   ${client.channels.size}`);
+console.log('---------------------------------');
+});
+client.on("message", message => {
 
-                            let yes = m.createReactionCollector(yesFilter);
-                            let no = m.createReactionCollector(noFiler);
-
-                            yes.on("collect", v => {
-                                m.delete();
-                                    message.channel.send(`:ballot_box_with_check: | Done ... The Broadcast Message Has Been Sent For ${message.guild.memberCount} Members`).then(msg => msg.delete(5000));
-                                        message.guild.members.forEach(member => {
-                                            let bc = new Discord.RichEmbed()
-                                            .setColor("RANDOM")
-                                            .setThumbnail(message.author.avatarURL)
-                                            .setTitle("Broadcast")
-                                            .addField("Server", message.guild.name)
-                                            .addField("Sender", message.author.username)
-                                            .addField("Message", args);
-
-                                            member.sendEmbed(bc);
-                                        });
-message.guild.members.forEach(m => { 
-m.send(args.replace('[user]', m));
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(argresult.replace('[user]', m));
 })
-                        });
-                        no.on("collect", v => {
-                            m.delete();
-                            message.channel.send("**Broadcast Canceled.**").then(msg => msg.delete(3000))
-                        });
-                            
-                        });
-            }
-            if(command == "bco") {
-                if(!message.member.hasPermission("ADMINISTRATOR")) {
-                    return message.channel.send("**للأسف لا تمتلك صلاحية `ADMINISTRATOR`**");
-                }
-                    if(!args) {
-                        return message.reply("**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**");
-                    }
-                        message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟\nمحتوى البرودكاست: \`${args}\`**`).then(m => {
-                            m.react("✅")
-                            .then(() => m.react("❌"));
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+if(message.content.startsWith(prefix + 'obc')) {  
+      let args = message.content.split(" ").slice(1);
+      var argresult = args.join(' '); 
+message.channel.send(`**:loudspeaker:  تم ارسال هذة الرسالة الى __${message.guild.memberCount}__ مشترك**`);
 
-                            let yesFilter = (reaction, user) => reaction.emoji.name == "✅" && user.id == message.author.id;
-                            let noFiler = (reaction, user) => reaction.emoji.name == "❌" && user.id == message.author.id;
-
-                            let yes = m.createReactionCollector(yesFilter);
-                            let no = m.createReactionCollector(noFiler);
-
-                            yes.on("collect", v => {
-                                m.delete();
-                                    message.channel.send(`:ballot_box_with_check: | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.filter(r => r.presence.status !== "offline").size} Members`).then(msg => msg.delete(5000));
-                                        message.guild.members.filter(r => r.presence.status !== "offline").forEach(member => {
-                                            let bco = new Discord.RichEmbed()
-                                            .setColor("RANDOM")
-                                            .setThumbnail(message.author.avatarURL)
-                                            .setTitle("Broadcast")
-                                            .addField("Server", message.guild.name)
-                                            .addField("Sender", message.author.username)
-                                            .addField("Message", args);
-
-                                            member.sendEmbed(bco);
-                                        });
 message.guild.members.forEach(m => { 
-m.send(args.replace('[user]', m));
-})
-                        });
-                        no.on("collect", v => {
-                            m.delete();
-                            message.channel.send("**Broadcast Canceled.**").then(msg => msg.delete(3000));
-                        });
-                            
-                        });
-            }
+m.send(argresult.replace('[user]', m));
+});
+}
+});
 
+client.on("message", message => {
 if(message.content.startsWith(prefix + "help")) {
 const embed = new Discord.RichEmbed() .setColor("RANDOM").setDescription(`**${prefix}obc ⇏ لإرسال رسالة إلى جميع أعضاء السيرفر
 ${prefix}bc ⇏ لإرسال رسالة إلى الأعضاء الأونلاين فقط
+${prefix}bcrole ⇏  لإرسآل رسالة لرتبة محدده 
+ex :   ${prefix}bcrole @admin message
 __تنبية__ : إذا أردت أن تمنشن العضو فقط أكتب بالرسالة
 \`[user]\` وسيقوم بإستبدالها بمنشن العضو**`);
 message.channel.sendEmbed(embed)}
-
 });
-
 client.login(process.env.BOT_TOKEN);
