18e6b866e4e6848b87d6b09161f043e9ef2fcaf4
osamagmt <34580348+osamagmt@users.noreply.github.com>
2019-03-07 23:15:42 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,35 @@
+const prefixbc = "$";
+client.on('message', async message => {  
+if(message.author.bot) return;
+if(message.channel.type === 'dm') return;
+if(!message.guild.members.get(message.author.id).hasPermission('ADMINISTRATOR')) return;
+const args = message.content.split(" ").slice(1);
+if(message.content.startsWith(prefixbc + 'obc')) {  
+message.channel.send(`**:loudspeaker:  تم ارسال هذة الرسالة الى __${message.guild.memberCount}__ مشترك**`);
+message.guild.members.forEach(m => { 
+m.send(args.replace('[user]', m));
+});}
+if(message.content.startsWith(prefixbc + 'bc')) {  
+message.channel.send(`**:loudspeaker:  تم ارسال هذة الرسالة الى ${message.guild.members.filter(m => m.presence.status !== 'online').size}__ مشترك**`);
+message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+m.send(args.replace('[user]', m));
+});}
+if(message.content.startsWith(prefixbc + 'obc')) {  
+var role = message.mentions.roles.first();
+if(!role) {message.reply("لا توجد رتبة بهذا الاسم");return;}
+if(!args[0]) {
+message.channel.send(`قم بمنشنة الرتبة  | ${prefixbc}bcrole @admin message`);
+return;}
+message.channel.send(`**:loudspeaker:  تم ارسال هذة الرسالة الى __${role.members.size}__ مشترك**`);
+message.guild.members.filter(m => m.roles.get(role.id)).forEach(n => {
+n.send(args[1].replace('[user]', n));});}
+if(message.content.startsWith(prefixbc + "help")) {
+const embed = new Discord.RichEmbed() .setColor("RANDOM").setDescription(`**${prefixbc}obc ⇏ لإرسال رسالة إلى جميع أعضاء السيرفر
+${prefixbc}bc ⇏ لإرسال رسالة إلى الأعضاء الأونلاين فقط
+${prefixbc}bcrole ⇏  لإرسآل رسالة لرتبة محدده 
+ex :   ${prefixbc}bcrole @admin message
+__تنبية__ : إذا أردت أن تمنشن العضو فقط أكتب بالرسالة
+\`[user]\` وسيقوم بإستبدالها بمنشن العضو**`);
+message.channel.sendEmbed(embed)}
+});
+client.login(process.env.BOT_TOKEN);
